f6e70dbda3e0b09894b4928feb2997bfe1ba0670
mikew <mikew123@gmail.com>
2026-09-17 17:27:12 -0500

    moved tof pcd topic creation from tof_node to pcd_node

--- src/robocolumbus_stuff/robocolumbus_stuff/tof_node.py
+++ src/robocolumbus_stuff/robocolumbus_stuff/tof_node.py
@@ -33,7 +33,7 @@ from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 class TofNode(Node):
     '''
     This processes the serial port from the RP2040 for the front and rear
-    TOF sensors
+    TOF sensors and creates tof_dist topics 
     '''
 
     timerRateHz = 100.0; # Rate to check serial port for messages
@@ -57,17 +57,6 @@ class TofNode(Node):
 
         self.openSerialPort()
 
-        # publish a topic for each TOF sensor fc=front_center etc
-        self.tof_fc_pcd_publisher = self.create_publisher(PointCloud2, 'tof_fc', 10)
-        self.tof_fl_pcd_publisher = self.create_publisher(PointCloud2, 'tof_fl', 10)
-        self.tof_fr_pcd_publisher = self.create_publisher(PointCloud2, 'tof_fr', 10)
-        self.tof_rc_pcd_publisher = self.create_publisher(PointCloud2, 'tof_rc', 10)
-        self.tof_rl_pcd_publisher = self.create_publisher(PointCloud2, 'tof_rl', 10)
-        self.tof_rr_pcd_publisher = self.create_publisher(PointCloud2, 'tof_rr', 10)
-        self.tof_fc_mid_publisher = self.create_publisher(Float32X8, 'tof_fc_mid', 10)
-
-        self.tof_dist_subscriber = self.create_subscription(TofDist, 'tof_dist'
-                                            , self.tof_dist_callback, 10)
 
         self.tof_dist_publisher = self.create_publisher(TofDist, 'tof_dist', 10)
       
@@ -184,9 +173,6 @@ class TofNode(Node):
         
         # publish raw data from sensors
         self.tof_sensor_publish(tof_ab, packet)
-
-        # # publish front center mid row point cloud    
-        # self.tof_pcd_publish(tof_ab, packet)
         
     # publish the "raw" TOF sensor distance data
     # nav_node uses the distance data instead of pcd
@@ -205,134 +191,6 @@ class TofNode(Node):
         msg.tof = tof_ab
         msg.dist = dist
         self.tof_dist_publisher.publish(msg)
-
-    # Process the TOF distance topics to create Point Clouds
-    # Each topic message hass the TOF sensor name as well as the 64 distance points
-    def tof_dist_callback(self, msg) -> None :
-    
-        tof_ab = msg.tof
-        data = msg.dist # 64 int16 list
-        self.tof_pcd_publish(tof_ab, data)
-
-    # AMCL uses pcd for obstical detection
-    # 8x8 point cloud for each sensor FOV 45degx45deg
-    # calculate x,y,z for each point
-    # TODO: Optimize math with numpy
-    def tof_pcd_publish(self, tof_ab, data) -> None:
-        # self.get_logger().info(f"tof_Publish: {tof_ab=} {data=}")
-
-        fov = 45.0
-        fovPt = fov/8 # FOV for each 8x8 sensor point
-        fovPtRad = fovPt*(math.pi/180) #scaled to Radians
-
-        # There is a curvature in the distances of the sensors that needs to be corrected
-        # Remove the curve by scaling each sensor with a inverted sin() curve over FOV
-        # NOTE: This could be pre-computed outside the function since it is constant
-        tofCurveCor = []
-        for n in range(0,8) :
-            theta:float = (n-4+0.5)*fovPtRad + math.pi/2
-            s:float = math.sin(theta)
-            if n == 0 : s0 = s
-            tofCurveCor.append(s0/s)            
-
-        # data is a liinear array of 64 values that needs to be converted to 8x8 array
-        dist = np.array(data).reshape(8, 8)
-
-        # pointcloud is a list of tupples(x,y,z)
-        xyz0:list = []
-        for m in range(0,8) : # Rows bottom to top
-            theta_m = (m-4+0.5)*fovPtRad
-            d_m = dist[m]
-            for n in range(0, 8) : # Collumns left to right
-                theta_n = (n-4+0.5)*fovPtRad
-                d = d_m[n]
-                if d==-1: 
-                    # Bad data - set as infinate number (use NaN instead?)
-                    xx0 = math.inf
-                    yy0 = math.inf
-                    zz0 = math.inf
-                else :
-                    Wx =  int(d*math.cos(theta_n)*tofCurveCor[n])
-                    Wy = -int(d*math.sin(theta_n)*tofCurveCor[n])
-                    Wz =  int(d*math.sin(theta_m)*tofCurveCor[n])
-                    # Convert mm to meters
-                    xx0 = np.float32(Wx/1000.0)
-                    yy0 = np.float32(Wy/1000.0)
-                    zz0 = np.float32(Wz/1000.0)
-                
-                xyz0.append((xx0,yy0,zz0))
-        
-        # self.get_logger().info(f"tof_Publish: {xyz0=}")
-
-        if tof_ab == "tof_fc" :
-            pcd = self.point_cloud(xyz0, 'tof_fc_link')
-            self.tof_fc_pcd_publisher.publish(pcd)
-        elif tof_ab == "tof_fl" :
-            pcd = self.point_cloud(xyz0, 'tof_fl_link')
-            self.tof_fl_pcd_publisher.publish(pcd)
-        elif tof_ab == "tof_fr" :
-            pcd = self.point_cloud(xyz0, 'tof_fr_link')
-            self.tof_fr_pcd_publisher.publish(pcd)
-        elif tof_ab == "tof_rc" :
-            pcd = self.point_cloud(xyz0, 'tof_rc_link')
-            self.tof_rc_pcd_publisher.publish(pcd)
-        elif tof_ab == "tof_rl" :
-            pcd = self.point_cloud(xyz0, 'tof_rl_link')
-            self.tof_rl_pcd_publisher.publish(pcd)
-        elif tof_ab == "tof_rr" :
-            pcd = self.point_cloud(xyz0, 'tof_rr_link')
-            self.tof_rr_pcd_publisher.publish(pcd)
-
-        if tof_ab == "tof_fc" :
-            # Publish the mid row distances as Float32X8 message for nav node
-            msg = Float32X8()
-            df = []
-            for d in dist[3] :
-                if d==-1 : d = math.inf
-                else : d /= 1000.0 # convert mm to meters
-                df.append(float(d))
-            msg.data = df
-            self.tof_fc_mid_publisher.publish(msg)
-
-    def point_cloud(self, points_xy:list[tuple[np.float32]], parent_frame:str="map") -> PointCloud2:
-        """
-            Input list of tuples (x,y,z)
-            Returns a point cloud to publish - Rviz can display it
-        """
-        points = np.asarray(points_xy)
-
-        ros_dtype = PointField.FLOAT32
-        dtype = np.float32
-        itemsize = np.dtype(dtype).itemsize # A 32-bit float takes 4 bytes.
-
-        data = points.astype(dtype).tobytes() 
-
-        # The fields specify what the bytes represents. The first 4 bytes 
-        # represents the x-coordinate, the next 4 the y-coordinate
-        fields = [PointField(
-            name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
-            for i, n in enumerate('xyz')]
-
-        #self.get_logger().info(f"{itemsize = } {fields = } {points = } {data = }")
-
-        # The PointCloud2 message also has a header which specifies which 
-        # coordinate frame it is represented in. 
-        header = Header(
-            frame_id=parent_frame,
-            stamp = self.get_clock().now().to_msg(),
-            )
-
-        return PointCloud2(
-            header=header,
-            height=1, 
-            width=points.shape[0],
-            is_dense=False,
-            is_bigendian=False, #Pi4
-            fields=fields,
-            point_step=(itemsize * 3), # Every point consists of two float32s.
-            row_step=(itemsize * 3 * points.shape[0]), 
-            data=data
-        )
 
 def main(args=None):
     rclpy.init(args=args)
